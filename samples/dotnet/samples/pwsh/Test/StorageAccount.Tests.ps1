8133c39f34e679024d33967be595248a9768e3cc
Dilmurod Makhamadaliev (Mak) <dilmurodm@microsoft.com>
2022-10-25 18:04:49 -0400

    storage account and bicep helpers

--- samples/dotnet/samples/pwsh/Test/StorageAccount.Tests.ps1
+++ samples/dotnet/samples/pwsh/Test/StorageAccount.Tests.ps1
@@ -4,58 +4,58 @@ $ROOT_PATH = $PSScriptRoot | split-path -parent | split-path -parent | split-pat
 BeforeAll {
     Import-Module -Name $ROOT_PATH/Benchpress/Helpers/Azure/StorageAcccount.psm1
     Import-Module -Name $ROOT_PATH/Benchpress/Helpers/Azure/Bicep.psm1
-
-    #declared at the top to be able to access by all tests functions
-    $storageAccountName = 'mystnamebicepv2'
-    $resourceGroupName = 'rg-test-bicep'
 }
+
+#global varaibles with required values for the tests
+$global:storageAccountName = 'mystnamebicepv2'
+$global:resourceGroupName = 'rg-test-bicep'
 
 Describe 'Spin up , Tear down Storage Account' {
     it 'Should deploy a bicep file.' {
 
-        #bicep file path
-        $bicepPath = "$ROOT_PATH/samples/dotnet/samples/pwsh/storageAccount.bicep"
+      #bicep file path
+      $bicepPath = "$ROOT_PATH/samples/dotnet/samples/pwsh/storageAccount.bicep"
 
-        #required parameters for storage account deployment
-        $params = @{
-          name = "mystnamebicepv2"
-          location = "eastus"
-        }
+      #required parameters for storage account deployment
+      $params = @{
+        name = "mystnamebicepv2"
+        location = "eastus"
+      }
 
-        #calling Deploy-BicepFeature helper to deploy resources
-        $deployment = Deploy-BicepFeature $bicepPath $params $resourceGroupName
+      #calling Deploy-BicepFeature helper to deploy resources
+      $deployment = Deploy-BicepFeature $bicepPath $params $resourceGroupName
 
-        #checking the return state from deployment
-        $deployment.ProvisioningState | Should -Be 'Succeeded'
+      #checking the return state from deployment
+      $deployment.ProvisioningState | Should -Be 'Succeeded'
     }
 
     it 'Should check storage account exist' {
-       #Calling StorageAccount helper method to check expected storage account exist
-       $storageAccount = Get-StorageAccountExists $resourceGroupName $storageAccountName
+      #Calling StorageAccount helper method to check expected storage account exist
+      $storageAccount = Get-StorageAccountExists $resourceGroupName $storageAccountName
 
-       #assert return value from StorageAccount helper
-       $storageAccount | Should -Not -BeNullOrEmpty -ErrorAction Stop
+      #assert return value from StorageAccount helper
+      $storageAccount | Should -Not -BeNullOrEmpty -ErrorAction Stop
     }
 
     it 'Should Check Storage Sku Name'{
 
-        #Calling StorageAccount helper method to check for expected storage Sku
-        $getStorageAccountSku = Get-StorageAccountSku $resourceGroupName $storageAccountName
+      #Calling StorageAccount helper method to check for expected storage Sku
+      $getStorageAccountSku = Get-StorageAccountSku $resourceGroupName $storageAccountName
 
-        #assert return value from StorageAccount helper
-        $getStorageAccountSku | Should -Be "Standard_LRS"
+      #assert return value from StorageAccount helper
+      $getStorageAccountSku | Should -Be "Standard_LRS"
     }
 
     it 'Should Check Storage Kind'{
-        #Calling StorageAccount helper method to check for expected storage Kind
-        $getStorageAccountKind = Get-StorageAccountKind $resourceGroupName $storageAccountName
+      #Calling StorageAccount helper method to check for expected storage Kind
+      $getStorageAccountKind = Get-StorageAccountKind $resourceGroupName $storageAccountName
 
-        #assert return value from StorageAccount helper
-        $getStorageAccountKind | Should -Be "StorageV2"
+      #assert return value from StorageAccount helper
+      $getStorageAccountKind | Should -Be "StorageV2"
     }
 
     it 'Delete Deployed Resources'{
-        #Call Bicep helper to remove deployed resources
-        Remove-BicepFeature $resourceGroupName
+      #Call Bicep helper to remove deployed resources
+      Remove-BicepFeature $resourceGroupName
     }
 }
