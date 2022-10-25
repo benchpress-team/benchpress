13bcd2a60ecc00250fe8adf9f7e6a042b3abdde9
Engin Polat <polatengin@hotmail.com>
2022-10-25 00:19:38 +0000

    adding initial dotnet test framework

--- framework/dotnet/BenchPress.TestFramework/Program.cs
+++ framework/dotnet/BenchPress.TestFramework/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+using BenchPress.TestEngine.Azure;
+
+namespace BenchPress.TestFramework.Azure
+{
+  public static class ResourceGroup
+  {
+      public static KeyValuePair<string, string> GetResourceGroup(string name)
+      {
+        var engine = new ResourceGroup();
+
+        return engine.GetResourceGroup(name);
+      }
+
+      public static KeyValuePair<string, string> GetResourceGroup(string name, string location)
+      {
+        var engine = new ResourceGroup();
+
+        return engine.GetResourceGroup(name, location);
+      }
+  }
+}
+
+/*
+sample usage;
+
+var fullResourceGroup1 = ResourceGroup.GetResourceGroup("rg-test");
+
+var fullResourceGroup2 = ResourceGroup.GetResourceGroup("rg-test", "eastus");
+
+var isResourceGroup1Exists = ResourceGroup.IsResourceGroupExists("rg-test");
+
+var isResourceGroup2Exists = ResourceGroup.IsResourceGroupExists("rg-test", "eastus");
+*/
