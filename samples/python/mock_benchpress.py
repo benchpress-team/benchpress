418a437c0bf97e44c799afc225beddc9d268c28e
Dilmurod Makhamadaliev (Mak) <dilmurodm@microsoft.com>
2022-10-31 15:08:59 -0400

    fixing python linting errors

--- samples/python/mock_benchpress.py
+++ samples/python/mock_benchpress.py
@@ -1,10 +1,13 @@
 def resource_group_exists(resource_group: str) -> bool:
+    print("ResourceGroup: " + resource_group)
     return True
 
 def create_resource_group(azure_subscription: str) -> str:
+    print("Subscription: " + azure_subscription)
     return "dummy_resource_group_id"
 
 def destroy(resource_group: str):
+    print("ResourceGroup: " + resource_group)
     pass
 
 if __name__ == "__main__":
